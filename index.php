267aaf9e6feea4a95884864688aad3ed0376b9e0
Hugo Gomez <hugo.gomez9795@gmail.com>
2017-09-22 14:10:59 +0200

    Creation of the form

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Connexion</title>
+  </head>
+  <body>
+    <fieldset>
+      <legend>Connexion :</legend>
+      <form action="/action_page.php" method="post">
+        Username : <input type="text" name="username"><br>
+        Password : <input type="text" name="password"><br>
+      <input type="submit" value="Submit">
+    </fieldset>
+</form>
+  </body>
+</html>
